5bb667d52fc18425e707bc6bbbc8f990db44ffce
Yago <andradeyago56@gmail.com>
2025-03-22 21:42:00 -0300

    (layout): Creating feed page

---
+++ clone-twitter-frontend/src/data/tweet.ts
@@ -0,0 +1,15 @@
+import { Tweet } from '@/types/tweet';
+import { user } from './user';
+
+export const tweet: Tweet = {
+  id: 123,
+  user: user,
+  body: 'Outro dia Magico',
+  image: '/postForY.png',
+  likeCount: 4123,
+  commentCount: 61,
+  retweetCount: 0,
+  liked: true,
+  retweeted: false,
+  dataPost: new Date(2024, 8, 1, 10, 0, 0),
+};
