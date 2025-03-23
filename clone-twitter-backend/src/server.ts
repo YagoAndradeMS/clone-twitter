0669e150d5ed251420480d888c2076b71feb9730
Yago <andradeyago56@gmail.com>
2025-03-23 12:49:47 -0300

    (first): project init

---
+++ clone-twitter-backend/src/server.ts
@@ -0,0 +1,15 @@
+import express, { urlencoded } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+const server = express();
+server.use(helmet());
+server.use(cors());
+server.use(urlencoded({ extended: true }));
+server.use(express.json());
+
+// rotas
+
+server.listen(process.env.PORT || 3001, () => {
+  console.log(`Servidor rodando na porta`, process.env.BASE_URL || '3001');
+});
