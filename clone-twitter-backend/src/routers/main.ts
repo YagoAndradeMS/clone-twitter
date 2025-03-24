8b5278c72177cd380546275723f69ff2cdbcd9f4
Yago <andradeyago56@gmail.com>
2025-03-24 10:03:01 -0300

    (routes): Creating route planning

---
+++ clone-twitter-backend/src/routers/main.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import * as pingController from '../controllers/ping.controller';
+
+export const mainRouter = Router();
+
+mainRouter.get('/ping', pingController.ping);
+// mainRouter.get('/private-ping', pingController.ping);
+
+// Rotas para login e registro
+mainRouter.post('/auth/signup');
+mainRouter.post('/auth/signin');
+
+// Rotas para tweets
+mainRouter.post('/tweet');
+mainRouter.get('/tweet/:id'); // tweet/123
+mainRouter.get('/tweet/:id/answers');
+mainRouter.post('/tweet/:id/like');
+
+// Rotas para os usuarios
+mainRouter.get('/user/:slug');
+mainRouter.get('/user/:slug/tweets');
+mainRouter.post('/user/:slug/follow');
+mainRouter.put('/user');
+mainRouter.put('/user/avatar');
+mainRouter.put('/user/cover');
+
+// Rotas do sistema
+mainRouter.get('/feed');
+mainRouter.get('/search');
+mainRouter.get('/trending');
+mainRouter.get('/suggestions');
--- clone-twitter-backend/src/server.ts
+++ clone-twitter-backend/src/server.ts
@@ -1,6 +1,7 @@
 import express, { urlencoded } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import { mainRouter } from './routers/main';
 
 const server = express();
 server.use(helmet());
@@ -9,6 +10,8 @@ server.use(urlencoded({ extended: true }));
 server.use(express.json());
 
 // rotas
+
+server.use(mainRouter);
 
 server.listen(process.env.PORT || 3001, () => {
   console.log(`Servidor rodando na porta`, process.env.BASE_URL || '3001');
