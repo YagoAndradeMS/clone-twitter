6828aba57d8504d41a7481d777249970f0dae324
Yago <andradeyago56@gmail.com>
2025-03-25 18:49:29 -0300

    (feat): Catching a tweet

--- clone-twitter-backend/src/routers/main.ts
+++ clone-twitter-backend/src/routers/main.ts
@@ -16,7 +16,7 @@ mainRouter.post('/auth/signin', authController.signin);
 
 // Rotas para tweets
 mainRouter.post('/tweet', verifyJWT, tweetController.addTweet);
-// mainRouter.get('/tweet/:id'); // tweet/123
+mainRouter.get('/tweet/:id', verifyJWT, tweetController.getTweet); // tweet/123
 // mainRouter.get('/tweet/:id/answers');
 // mainRouter.post('/tweet/:id/like');
 
