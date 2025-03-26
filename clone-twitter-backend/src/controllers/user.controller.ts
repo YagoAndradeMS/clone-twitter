0f5fabe460299acaf29fc9d44338b97d1bb563c1
Yago <andradeyago56@gmail.com>
2025-03-25 21:38:43 -0300

    (feat): Adding functions to update users and follow users

--- clone-twitter-backend/src/controllers/user.controller.ts
+++ clone-twitter-backend/src/controllers/user.controller.ts
@@ -1,13 +1,18 @@
 import { Response } from 'express';
 import { ExtendedRequest } from '../../types/extended-request';
 import {
+  checkIfFollows,
   findUserBySlug,
+  follow,
   getUserFollowersCount,
   getUserFollowingCount,
   getUserTweetCount,
+  unfllow,
+  updateUserInfo,
 } from '../services/user.service';
 import { userTweetsSchema } from '../schemas/user-tweets';
 import { findTweetsByUser } from '../services/tweet.service';
+import { updateUserSchema } from '../schemas/update-user';
 
 export const getUser = async (req: ExtendedRequest, res: Response) => {
   const { slug } = req.params;
@@ -41,4 +46,37 @@ export const getUserTweets = async (req: ExtendedRequest, res: Response) => {
   const tweets = await findTweetsByUser(slug, currentPage, perPage);
 
   res.json({ tweets, page: currentPage });
+};
+
+export const followToogle = async (req: ExtendedRequest, res: Response) => {
+  const { slug } = req.params;
+  const user = req.userSlug as string;
+
+  const hasUserToBeFollowed = await findUserBySlug(slug);
+  if (!hasUserToBeFollowed) {
+    res.json({ error: 'Usuario inexistenet' });
+    return;
+  }
+
+  const follows = await checkIfFollows(user, slug);
+  if (!follows) {
+    await follow(user, slug);
+    res.json({ follow: true });
+  } else {
+    await unfllow(user, slug);
+    res.json({ follow: true });
+  }
+};
+
+export const updateUser = async (req: ExtendedRequest, res: Response) => {
+  // Validar os dados enviados
+  const safeData = updateUserSchema.safeParse(req.body);
+  if (!safeData.success) {
+    res.json({ error: safeData.error.flatten().fieldErrors });
+    return;
+  }
+
+  await updateUserInfo(req.userSlug as string, safeData.data);
+
+  res.json('Atualização executada com sucesso');
 };
--- clone-twitter-backend/src/routers/main.ts
+++ clone-twitter-backend/src/routers/main.ts
@@ -24,8 +24,8 @@ mainRouter.post('/tweet/:id/like', verifyJWT, tweetController.likeToggle);
 // Rotas para os usuarios
 mainRouter.get('/user/:slug', verifyJWT, userController.getUser);
 mainRouter.get('/user/:slug/tweets', verifyJWT, userController.getUserTweets);
-// mainRouter.post('/user/:slug/follow');
-// mainRouter.put('/user');
+mainRouter.post('/user/:slug/follow', verifyJWT, userController.followToogle);
+mainRouter.put('/user', verifyJWT, userController.updateUser);
 // mainRouter.put('/user/avatar');
 // mainRouter.put('/user/cover');
 
